dfc78ca8e080cf6010cf8e7fdd36f60b700dc730
RohanChikkalkar <rithikrohan007@gmail.com>
2024-01-27 19:37:47 +0530

    add ui

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+let playerWins = 0;
+let computerWins = 0;
+let rockButton = document.querySelector(".rock");
+let paperButton = document.querySelector(".paper");
+let scissorsButton = document.querySelector(".scissors");
+let results = document.querySelector(".round-results");
+let playerScore = document.querySelector("#playerScore");
+let computerScore = document.querySelector("#computerScore");
+let gameResult = document.querySelector(".game-header");
+
+rockButton.addEventListener("click", () => {
+  game("rock");
+});
+
+paperButton.addEventListener("click", () => {
+  game("paper");
+});
+
+scissorsButton.addEventListener("click", () => {
+  game("scissors");
+});
+
+function game(playerChoice) {
+  let winner = playRound(playerChoice, getComputerChoice());
+  if (winner != "tie") {
+    if (winner == "player") {
+      playerWins++;
+      playerScore.textContent = `Player Score : ${playerWins}`;
+    } else if (winner == "computer") {
+      computerWins++;
+      computerScore.textContent = `Computer Score : ${computerWins}`;
+    }
+  } else if (winner == "tie") {
+    let tieGame = playRound(playerChoice, getComputerChoice());
+    if (tieGame == "player") {
+      playerWins++;
+      playerScore.textContent = `Player Score : ${playerWins}`;
+    } else if (tieGame == "computer") {
+      computerWins++;
+      computerScore.textContent = `Computer Score : ${computerWins}`;
+    }
+  }
+
+  // Check if the game has ended
+  if (playerWins == 5 || computerWins == 5) {
+    declareWinner();
+    // Reset the game
+    playerWins = 0;
+    computerWins = 0;
+    playerScore.textContent = `Player Score : ${playerWins}`;
+    computerScore.textContent = `Computer Score : ${computerWins}`;
+  }
+}
+
 // This function gets the computer's choice
 
 function getComputerChoice() {
@@ -13,66 +67,34 @@ function getComputerChoice() {
 //This function plays a single round of the game and returns a user the result
 
 function playRound(playerSelection, computerSelection) {
-  let player = playerSelection.toLowerCase();
-
-  if (player == computerSelection) {
-    console.log("it's a tie!!! lets do a rematch");
+  if (playerSelection == computerSelection) {
+    results.textContent = "it's a tie!!! lets do a rematch";
     return "tie";
-  } else if (player == "rock" && computerSelection == "scissors") {
-    console.log("You Win! Rock beats scissors");
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    results.textContent = "You Win! Rock beats scissors";
     return "player";
-  } else if (player == "scissors" && computerSelection == "rock") {
-    console.log("Computer won! Rock beats scissors");
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    results.textContent = "Computer won! Rock beats scissors";
     return "computer";
-  } else if (player == "scissors" && computerSelection == "paper") {
-    console.log("You win! Scissors beat paper");
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    results.textContent = "You win! Scissors beat paper";
     return "player";
-  } else if (player == "paper" && computerSelection == "scissors") {
-    console.log("Computer won! Scissors beat paper");
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    results.textContent = "Computer won! Scissors beat paper";
     return "computer";
-  } else if (player == "paper" && computerSelection == "rock") {
-    console.log("You win! Paper beats rock");
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    results.textContent = "You win! Paper beats rock";
     return "player";
-  } else if (player == "rock" && computerSelection == "paper") {
-    console.log("Computer won! paper beats rock");
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    results.textContent = "Computer won! paper beats rock";
     return "computer";
   }
 }
 
-function game() {
-  playerSelection = prompt("what is your choice");
-  computerSelection = getComputerChoice();
-  return playRound(playerSelection, computerSelection);
-}
-
-//declaring points and winner
-
-function declare_winner() {
-  let playerWins = 0;
-  let computerWins = 0;
-  for (let i = 0; i < 5; i++) {
-    let winner = game();
-    if (winner != "tie") {
-      if (winner == "player") {
-        playerWins++;
-      } else if (winner == "computer") {
-        computerWins++;
-      }
-    } else if (winner == "tie") {
-      let tieGame = game();
-      if (tieGame == "player") {
-        playerWins++;
-      } else if (tieGame == "computer") {
-        computerWins++;
-      }
-    }
-  }
-
-  if (playerWins == 3) {
-    console.log(`player wins! with score ${playerWins}`);
-  } else if (computerWins == 3) {
-    console.log(`Computer Wins! with score ${computerWins}`);
+function declareWinner() {
+  if (playerWins == 5) {
+    gameResult.textContent = "Player is the final winner!";
+  } else if (computerWins == 5) {
+    gameResult.textContent = "Computer is the final winner!";
   }
 }
-
-declare_winner();
